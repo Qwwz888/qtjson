a6b697ea34ee7ce342852138ddb377e49dcb153a
ZhangJun <zjnt888@outlook.com>
2022-10-19 11:58:35 +0800

    hi

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <QJsonArray> // [ ]
 #include <QJsonDocument> // 解析Json
 #include <QJsonValue> // int float double bool null { } [ ]
-//#include <QJsonParseError>
+#include <QJsonParseError>
 #include <QFile>
 
 
